aa656c13c76b70c815263ffe05452402d8d152fc
tba-adnan <tebbaa.adnane.dev@gmail.com>
2023-06-06 17:02:15 +0100

    ToAdd Couroutines.

--- MobileApp/app/src/main/java/com/example/libalex/MainActivity.kt
+++ MobileApp/app/src/main/java/com/example/libalex/MainActivity.kt
@@ -1,17 +1,18 @@
-package com.example.libalex
-import BooksApiClient
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
-import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
 import com.example.libalex.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var searchEditText: EditText
@@ -38,18 +39,33 @@ class MainActivity : AppCompatActivity() {
 
     private fun searchBooks(query: String) {
         val maxResults = 10
-        val books = booksApiClient.searchBooks(query, maxResults)
 
-        val adapter = ArrayAdapter(
-            this,
-            android.R.layout.simple_list_item_1,
-            books.map { it.title }
-        )
-        booksListView.adapter = adapter
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val response = booksApiClient.searchBooks(query, maxResults)
+                withContext(Dispatchers.Main) {
+                    if (response.isSuccessful) {
+                        val books = response.body()
+                        if (books != null) {
+                            val adapter = ArrayAdapter(
+                                this@MainActivity,
+                                android.R.layout.simple_list_item_1,
+                                books.map { it.title }
+                            )
+                            booksListView.adapter = adapter
 
-        booksListView.setOnItemClickListener { _, _, position, _ ->
-            val book = books[position]
-            // Handle book selection
+                            booksListView.setOnItemClickListener { _, _, position, _ ->
+                                val book = books[position]
+                                // Handle book selection
+                            }
+                        }
+                    } else {
+                        // Handle error response
+                    }
+                }
+            } catch (e: Exception) {
+                // Handle network failure
+            }
         }
     }
 }
